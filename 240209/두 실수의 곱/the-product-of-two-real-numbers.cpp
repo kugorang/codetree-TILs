bdf0050aeb2d5fc1550bf894b319e951f924e3df
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-09 01:00:12 +0900

    Title: 두 실수의 곱, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240209/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float a = 5.26, b = 8.27;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << (float)a * b << endl;
+
+    return 0;
+}
