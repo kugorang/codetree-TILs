28dc98d6ec1becebab89c17b3881dc323a5ab1da
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-09 01:13:15 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240209/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1, b = 2, c = 3;
+    int sum = a + b + c;
+
+    a = sum;
+    b = sum;
+    c = sum;
+
+    cout << a << " " << b << " " << c;
+
+    return 0;
+}
