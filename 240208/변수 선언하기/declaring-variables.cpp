bb0777fc1959ae225c4c0f4a547a5da5cbff7ef2
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-08 23:15:56 +0900

    Title: 변수 선언하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/변수 선언하기/declaring-variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    char b;
+
+    a = 3;
+    b = 'C';
+
+    cout << a << endl << b;
+
+    return 0;
+}
