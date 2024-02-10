516841efe63b862d6e1c7260bf83095ef94f87c2
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-10 19:31:34 +0900

    Title: 길이 단위 환산하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float n;
+
+    cin >> n;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << n * 30.48;
+
+    return 0;
+}
