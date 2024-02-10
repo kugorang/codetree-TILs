0852f3517ac6ccba229ac5d63ade703a6b8484fb
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-10 19:54:40 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int frontNumbers, backNumbers;
+    char hyphen;
+
+    cin >> frontNumbers >> hyphen >> backNumbers;
+
+    cout << frontNumbers << backNumbers;
+
+    return 0;
+}
