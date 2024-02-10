356aebe25be74441d85ef6492304540ccf52d678
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-11 06:01:14 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240211/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int score;
+
+    cin >> score;
+
+    cout << (score == 100 ? "pass" : "failure");
+
+    return 0;
+}
