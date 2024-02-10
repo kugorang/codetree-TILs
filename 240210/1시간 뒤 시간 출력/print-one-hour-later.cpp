2f1811d773943769e38bfe43d714c4ecd0207052
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-10 19:49:36 +0900

    Title: 1시간 뒤 시간 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int h, m;
+    char tmp;
+
+    cin >> h >> tmp >> m;
+
+    cout << h + 1 << tmp << m;
+
+    return 0;
+}
