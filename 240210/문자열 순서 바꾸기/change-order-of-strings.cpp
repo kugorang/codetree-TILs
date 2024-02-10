be569c5cf18bbdf7bca8103dd1690657fbf647ea
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-10 19:46:38 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s, t;
+
+    cin >> s;
+    cin >> t;
+
+    cout << t << endl << s;
+
+    return 0;
+}
