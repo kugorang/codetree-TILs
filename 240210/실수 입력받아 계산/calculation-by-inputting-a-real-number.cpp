8d2559d1adb7658dd231d2a44dceed694b6bbd13
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-10 19:38:53 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float a, b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a + b;
+
+    return 0;
+}
