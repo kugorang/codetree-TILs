269129247444e9ca996ae0cc60c0cc03c8217637
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-12 11:15:04 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/비교 연산/comparison-operator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << (a >= b ? 1 : 0) << endl
+        << (a > b ? 1 : 0) << endl
+        << (b >= a ? 1 : 0) << endl
+        << (b > a ? 1 : 0) << endl
+        << (a == b ? 1 : 0) << endl
+        << (a != b ? 1 : 0);
+
+    return 0;
+}
