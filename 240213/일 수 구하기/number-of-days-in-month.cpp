43e7cd90f0cc08f12c6fd24cbc06691875feca0a
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-13 16:41:15 +0900

    Title: 일 수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/일 수 구하기/number-of-days-in-month.cpp
+++ 240213/일 수 구하기/number-of-days-in-month.cpp
@@ -14,10 +14,11 @@ int main() {
             days = 28;
             break;
         case 1: case 3: case 5:
-        case 8: case 10: case 12:
+        case 7: case 8: case 10:
+        case 12:
             days = 31;
             break;
-        case 4: case 6: case 7:
+        case 4: case 6:
         case 9: case 11:
             days = 30;
             break;
