1231e3288acae9c221b1e3b00604e159dd58ec59
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-14 13:16:58 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240214/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    cin >> a >> b;
+
+    if (a >= 1)
+    {
+        for (int i = 1; i <= b; i++)
+        {
+            cout << a;
+        }
+    }
+    else
+    {
+        cout << 0;
+    }
+
+    return 0;
+}
