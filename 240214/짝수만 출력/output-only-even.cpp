a728f85158b5ba073341ce73f866ee72bb48b2ab
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-14 10:01:48 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240214/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    cin >> a >> b;
+
+    int i = a;
+
+    while (i <= b)
+    {
+        cout << i << ' ';
+
+        i += 2;
+    }
+
+    return 0;
+}
