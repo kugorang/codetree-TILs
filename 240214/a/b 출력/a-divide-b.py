742ae092f9adf712a27df87ef2d23f675ef9e393
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-14 11:12:49 +0900

    Title: a/b 출력, Time: 337ms, Memory: 29MB, Status: Passed - CodeTree

--- 240214/a/b 출력/a-divide-b.py
+++ 240214/a/b 출력/a-divide-b.py
@@ -1,6 +1,15 @@
 import sys
-from decimal import Decimal
+from decimal import Decimal, getcontext, ROUND_DOWN
+
+# 정밀도를 21로 설정 (소수점 아래 20자리까지 정확하게 계산)
+getcontext().prec = 21
 
 a, b = map(int, sys.stdin.readline().split())
 
-print("{:.20f}".format(Decimal(a) / b))
+# a 나누기 b 계산
+result = Decimal(a) / Decimal(b)
+
+# 소수점 아래 20자리까지 버림
+rounded_result = result.quantize(Decimal('1.00000000000000000000'), rounding=ROUND_DOWN)
+
+print(f"{rounded_result}")
