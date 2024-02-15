88b7c45c0aec3f7ef9b8cf86c2d819422bed87dd
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-15 10:47:35 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240215/a의 b승/a-to-the-power-of-b.cpp
+++ 240215/a의 b승/a-to-the-power-of-b.cpp
@@ -9,7 +9,14 @@ int main() {
 
     cin >> a >> b;
 
-    cout << pow(a, b);
+    int multiple = 1;
+
+    for (int i = 0; i < b; i++)
+    {
+        multiple *= a;
+    }
+
+    cout << multiple;
 
     return 0;
 }
