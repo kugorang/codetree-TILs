036b114534b0db07288544e7d534312e4cb6258f
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-15 10:41:56 +0900

    Title: 입력받은 수의 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240215/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    cin >> n;
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int inputNum;
+
+        cin >> inputNum;
+
+        sum += inputNum;
+    }
+
+    cout << fixed;
+    cout.precision(1);
+    
+    cout << sum << ' ' << (float)sum / n;
+
+    return 0;
+}
