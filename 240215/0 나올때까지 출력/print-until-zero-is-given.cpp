743ae70c624980c70d40ead997beaddcba526e0f
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-15 11:16:39 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240215/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    while (true)
+    {
+        int inputNum;
+
+        cin >> inputNum;
+
+        if (inputNum == 0)
+        {
+            break;
+        }
+
+        cout << inputNum << endl;
+    }
+
+    return 0;
+}
