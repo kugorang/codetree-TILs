3279b7c8008d2382e64794e3e875d6d2c9c23f2c
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-17 10:54:11 +0900

    Title: 2의 거듭제곱수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240217/2의 거듭제곱수/pow-of-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int inputNum;
+
+    cin >> inputNum;
+
+    int count = 0;
+
+    while (inputNum > 1)
+    {
+        inputNum /= 2;
+        count++;
+    }
+
+    cout << count;
+
+    return 0;
+}
