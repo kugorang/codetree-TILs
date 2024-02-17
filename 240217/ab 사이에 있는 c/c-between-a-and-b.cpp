d8833dd1c8d4eb17a838b87689b817ecdc132947
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-17 10:59:49 +0900

    Title: ab 사이에 있는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240217/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    string output = "NO";
+
+    for (int i = a; i <= b; i++)
+    {
+        if (i % c == 0)
+        {
+            output = "YES";
+            break;
+        }
+    }
+
+    cout << output;
+
+    return 0;
+}
