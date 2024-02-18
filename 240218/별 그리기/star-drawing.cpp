ef37f1642bb17b79dfe7d2699c2911b5e51349fe
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-18 19:44:08 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240218/별 그리기/star-drawing.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - i - 1; j > 0; j--)
+        {
+            cout << ' ';
+        }
+
+        for (int j = 0; j <= 2 * i; j++)
+        {
+            cout << '*';
+        }
+
+        cout << endl;
+    }
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << ' ';
+        }
+
+        for (int j = 0; j < 2 * i - 1; j++)
+        {
+            cout << '*';
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
