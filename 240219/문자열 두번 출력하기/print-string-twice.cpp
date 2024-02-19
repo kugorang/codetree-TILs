47f49836cc25e05091f3d27b1bc055fbd1ba3d76
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-19 23:39:36 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240219/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string inputString;
+
+    cin >> inputString;
+
+    for (int i = 0; i < 2; i++)
+    {
+        cout << inputString << endl;
+    }
+
+    return 0;
+}
