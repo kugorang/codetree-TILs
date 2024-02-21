fb1c33a43f248a250bd437665f47a924380f7793
Kim Hyeonwoo <ialskdji@gmail.com>
2024-02-21 23:51:10 +0900

    Title: 가장 짧은 문자열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240221/가장 짧은 문자열/shortest-string.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string inputStr;
+    string longestStr;
+    string shortestStr;
+
+    cin >> inputStr;
+    longestStr = inputStr;
+    shortestStr = inputStr;
+
+    for (int i = 1; i < 3; i++)
+    {
+        cin >> inputStr;
+
+        if (inputStr.length() > longestStr.length())
+        {
+            longestStr = inputStr;
+        }
+        else if (shortestStr.length() > longestStr.length())
+        {
+            shortestStr = inputStr;
+        }
+    }
+
+    cout << longestStr.length() - shortestStr.length();
+
+    return 0;
+}
