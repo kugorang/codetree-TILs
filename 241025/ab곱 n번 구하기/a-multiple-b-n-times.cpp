75b7db300e956219e3b9059fb1f2f047a7882ad6
Kim Hyeonwoo <ialskdji@gmail.com>
2024-10-25 11:49:42 +0900

    Title: ab곱 n번 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241025/ab곱 n번 구하기/a-multiple-b-n-times.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int a, b;
+
+        cin >> a >> b;
+
+        int result = 1;
+
+        for (int j = a; j <= b; ++j)
+            result *= j;
+
+        cout << result << endl;
+    }
+
+    return 0;
+}
