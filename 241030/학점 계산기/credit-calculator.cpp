e07e0c405ad5d9a1d4c898a3d3c38f6d756ef61d
Kim Hyeonwoo <ialskdji@gmail.com>
2024-10-30 14:53:26 +0900

    Title: 학점 계산기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241030/학점 계산기/credit-calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    float scoreSum = .0f;
+
+    for (int i = 0; i < n; ++i)
+    {
+        float currentScore;
+        cin >> currentScore;
+
+        scoreSum += currentScore;
+    }
+
+    float scoreAverage = scoreSum / n;
+    string scoreResult;
+
+    if (scoreAverage > 4.0f)
+        scoreResult = "Perfect";
+    else if (scoreAverage > 3.0f)
+        scoreResult = "Good";
+    else
+        scoreResult = "Poor";
+
+    cout.precision(1);
+    cout << fixed << scoreSum / n << endl << scoreResult;
+
+    return 0;
+}
