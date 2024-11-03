ad831a2e3e00b460b84caa39a54f95f29bb49c25
Kim Hyeonwoo <ialskdji@gmail.com>
2024-11-03 10:47:34 +0900

    Title: 배열의 특정 위치 찾기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241103/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int oddSum = 0;
+    int evenSum = 0;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        int inputNum;
+
+        cin >> inputNum;
+
+        if (inputNum % 2)
+            oddSum += inputNum;
+        else
+            evenSum += inputNum;
+    }
+
+    cout << oddSum >= evenSum ? oddSum - evenSum : evenSum - oddSum;
+
+    return 0;
+}
