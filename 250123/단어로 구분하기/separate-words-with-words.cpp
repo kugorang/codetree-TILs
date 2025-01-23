092b7621153d3a0377472f1831d52c57345c86ca
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-23 11:51:01 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250123/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 10; ++i)
+    {
+        string str;
+        cin >> str;
+
+        cout << str << endl;
+    }
+    return 0;
+}
