11a7b55eab949d7e6c7d6cd9a3f9892e8e3a347d
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-29 16:36:16 +0900

    Title: 단어로 구분하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250129/단어로 구분하기 3/separate-words-with-words-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string arr[10];
+
+    for (int i = 0; i < 10; ++i)
+        cin >> arr[9 - i];
+    
+    for (int i = 0; i < 10; ++i)
+        cout << arr[i] << endl;
+
+    return 0;
+}
