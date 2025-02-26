5a92d244f7582368f432ea0117aa4c412f242042
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-26 11:58:59 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250226/Date to Date/date-to-date.cpp
+++ 250226/Date to Date/date-to-date.cpp
@@ -8,16 +8,19 @@ int main() {
     cin >> m1 >> d1 >> m2 >> d2;
 
     int numOfDays[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
     int answer1 = 0, answer2 = 0;
 
-    for (int i = 1; i <= m1; ++i)
+    for (int i = 1; i < m1; ++i)
         answer1 += numOfDays[i];
     
-    for (int i = 1; i <= m2; ++i)
+    answer1 += d1;
+    
+    for (int i = 1; i < m2; ++i)
         answer2 += numOfDays[i];
 
-    cout << answer2 + d2 - (answer1 + d1);
+    answer2 += d2;
+
+    cout << answer2 - answer1 + 1;
 
     return 0;
 }
