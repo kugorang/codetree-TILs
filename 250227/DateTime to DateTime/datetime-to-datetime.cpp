9eb378de7fbe0b12aea5711ef72a75b9b2783e40
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-27 13:31:17 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250227/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250227/DateTime to DateTime/datetime-to-datetime.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 int main() 
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int a, b, c;
 
     cin >> a >> b >> c;
@@ -15,15 +11,13 @@ int main()
     int a2 = 11, b2 = 11, c2 = 11;
 
     if (a < a2)
-        return -1;
-    
-    if (b < b2)
-        return -1;
-    
-    if (c < c2)
-        return -1;
-        
-    cout << (a * 24 * 60 + b * 60 + c) - (a2 * 24 * 60 + b2 * 60 + c2);
+        cout << -1;
+    else if (b < b2)
+        cout << -1;
+    else if (c < c2)
+        cout << -1;
+    else        
+        cout << (a * 24 * 60 + b * 60 + c) - (a2 * 24 * 60 + b2 * 60 + c2);
 
     return 0;
 }
