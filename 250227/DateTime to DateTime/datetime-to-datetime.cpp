0ef14683d2202b1bf74928d87c3bb40853061046
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-27 13:21:28 +0900

    Title: DateTime to DateTime, Time: 1043ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 250227/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    int a2 = 11, b2 = 11, c2 = 11;
+    int count = 0;
+
+    while (!(a == a2 && b == b2 && c == c2))
+    {
+        ++c2;
+
+        if (c2 == 60)
+        {
+            ++b2;
+            c2 = 0;
+
+            if (b2 == 24)
+            {
+                ++a2;
+                b2 = 0;
+            }
+        }
+
+        ++count;
+    }
+
+    cout << count;
+
+    return 0;
+}
