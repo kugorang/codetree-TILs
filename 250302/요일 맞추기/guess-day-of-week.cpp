f339c1dfc0270c70a09c36f85fe09f09f3dc3053
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-02 19:14:03 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250302/요일 맞추기/guess-day-of-week.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m1, d1, m2, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int daysOfMonths[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    string dayOfWeek1[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    string dayOfWeek2[] = {"Mon", "Sun", "Sat", "Fri", "Thu", "Wed", "Tue"};
+
+    int count1 = d1;
+    
+    for (int i = 1; i <= m1; ++i)
+        count1 += daysOfMonths[i];
+
+    int count2 = d2;
+
+    for (int i = 1; i <= m2; ++i)
+        count2 += daysOfMonths[i];
+
+    if (count2 > count1)
+        cout << dayOfWeek1[count2 - count1];
+    else
+        cout << dayOfWeek2[count1 - count2];
+
+    return 0;
+}
