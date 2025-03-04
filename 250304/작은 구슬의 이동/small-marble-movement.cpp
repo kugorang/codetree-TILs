bc1483ddc0141c7eec13d646bd3c17261bed125d
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-04 16:31:32 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250304/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int n;
+
+bool InRange(int x, int y)
+{
+    return (x > 0 && x <= n && y > 0 && y <= n);
+}
+
+int main() {
+    int t;
+    cin >> n >> t;
+    
+    int r, c;
+    char d;
+    cin >> r >> c >> d;
+    
+    unordered_map<char, int> um = {
+        { 'R', 0 },
+        { 'D', 1 },
+        { 'L', 3 },
+        { 'U', 2 }
+    };
+
+    int dx[] = { 1, 0, 0, -1 };
+    int dy[] = { 0, 1, -1, 0 };
+
+    for (int i = 0; i < t; ++i)
+    {
+        if (InRange(c + dx[um[d]], r + dy[um[d]]))
+        {
+            c += dx[um[d]];
+            r += dy[um[d]];
+        }
+        else
+            d = 3 - d;
+    }
+
+    cout << r << ' ' << c;
+
+    return 0;
+}
