052fd2c734b701b4514f093b786856fea1c3f80d
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-04 23:33:51 +0900

    Title: 되돌아오기 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250304/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string commands;
+
+int main() {
+    cin >> commands;
+
+    int commandsLength = commands.length();
+
+    int dx[] = { 1, 0, -1, 0 };
+    int dy[] = { 0, -1, 0, 1 };
+
+    int x = 0, y = 0, direction = 3, count = 0;
+
+    for (int i = 0; i < commandsLength; ++i)
+    {
+        switch (commands[i])
+        {
+            case 'L':
+                direction = (direction + 1) % 4;
+                break;
+            case 'R':
+                direction = (direction + 3) % 4;
+                break;
+            case 'F':
+                x += dx[direction];
+                y += dy[direction];
+                break;
+        }
+
+        ++count;
+
+        if (x == 0 && y == 0)
+            break;
+    }
+
+    cout << (x == 0 && y == 0 ? count : -1);
+
+
+    return 0;
+}
