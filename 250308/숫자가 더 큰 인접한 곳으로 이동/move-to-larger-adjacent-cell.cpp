fd686ebb8cb3690c8d5b03a11e0c5829ea9a3923
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-08 23:37:45 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250308/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n;
+
+bool InRange(int x, int y)
+{
+    return (x >= 1 && x <= n && y >= 1 && y <= n);
+}
+
+int main() {
+    int r, c;
+    cin >> n >> r >> c;
+
+    int a[101][101];
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            cin >> a[i][j];
+
+    int dx[] = { 0, 0, -1, 1 };
+    int dy[] = { 1, -1, 0, 0 };
+
+    int maxValue = INT_MIN;
+
+    cout << a[r][c] << ' ';
+
+    while (true)
+    {
+        int currentRow = r;
+        int currentCol = c;
+
+        for (int i = 0; i < 4; ++i)
+        {
+            if (!InRange(r + dy[i], c + dx[i]))
+                continue;
+
+            if (a[r + dy[i]][c + dx[i]] > a[r][c])
+            {
+                r += dy[i];
+                c += dx[i];
+
+                maxValue = a[r][c];
+
+                cout << maxValue << ' ';
+                break;
+            }
+        }
+
+        if (currentRow == r && currentCol == c)
+            break;
+    }
+
+    return 0;
+}
