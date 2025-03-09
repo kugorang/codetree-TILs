fe86afaa7c7746403243f2724f2dafc285c9ac60
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-09 20:15:54 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250309/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+bool InRange(int x, int y)
+{
+    return (x >= 1 && x <= n && y >= 1 && y <= n);
+}
+
+int main()
+{
+    int m, t;
+    cin >> n >> m >> t;
+
+    int arr[21][21];
+    int count[21][21] = { 0, };
+
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= n; ++j)
+            cin >> arr[i][j];
+
+    for (int i = 1; i <= m; ++i)
+    {
+        int r, c;
+        cin >> r >> c;
+
+        count[r][c] = 1;
+    }
+
+    // 상하좌우 순서
+    int dx[] = { 0, 0, -1, 1 };
+    int dy[] = { -1, 1, 0, 0 };
+
+    for (int i = 0; i < t; ++i)
+    {
+        for (int row = 1; row <= n; ++row)
+        {
+            for (int col = 1; col <= n; ++col)
+            {
+                if (count[row][col] == 0)
+                    continue;
+
+                for (int k = 0; k < 4; ++k)
+                {
+                    if (!InRange(row + dy[k], col + dx[k]))
+                        continue;
+
+                    if (arr[row + dy[k]][col + dx[k]] > arr[row][col])
+                    {
+                        count[row][col] -= 1;
+                        count[row + dy[k]][col + dx[k]] += 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int row = 1; row <= n; ++row)
+            for (int col = 1; col <= n; ++col)
+                if (count[row][col] >= 2)
+                    count[row][col] = 0;
+    }
+
+    int answer = 0;
+
+    for (int row = 1; row <= n; ++row)
+        for (int col = 1; col <= n; ++col)
+            if (count[row][col] == 1)
+                ++answer;
+
+    cout << answer;
+
+    return 0;
+}
