463a060e76db8f22ea87b959fab35e2201adf9f3
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-16 07:39:26 +0900

    Title: 각 자리 숫자들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/각 자리 숫자들의 합/sum-of-each-digit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int answer = 0;
+
+    while (n > 0)
+    {
+        answer += n % 10;
+        n /= 10;
+    }
+
+    cout << answer;
+
+    return 0;
+}
