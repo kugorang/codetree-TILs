623cb7cf9082720da789d1ce3aeab4b8963b92cb
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-23 12:05:52 +0900

    Title: 두 개의 동일한 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250423/두 개의 동일한 수열/two-equal-series.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int B[100];
+
+int main() {
+    int n;
+    cin >> n;
+    
+
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> B[i];
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
