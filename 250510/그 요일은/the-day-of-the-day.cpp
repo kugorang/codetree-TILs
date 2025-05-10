cafa36507dd1a6e68f667247decc39d2ad8d0d5a
Kim Hyeonwoo <ialskdji@gmail.com>
2025-05-10 16:07:02 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250510/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string day[] = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+    int month[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    int m1, m2, d1, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+    
+    string A;
+    cin >> A;
+
+    int answer = 0;
+    int loopCount = 0;
+
+    while (m1 < m2 || d1 <= d2)
+    {
+        if (day[loopCount % 7] == A)
+            ++answer;
+        
+        ++loopCount;
+
+        if (++d1 == month[m1])
+        {
+            d1 = 1;
+            ++m1;
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
