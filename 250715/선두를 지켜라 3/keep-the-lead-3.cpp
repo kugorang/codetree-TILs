80eb839664145fd6659c1b1d16057edfab1dabd9
Kim Hyeonwoo <ialskdji@gmail.com>
2025-07-15 22:04:48 +0900

    Title: 선두를 지켜라 3, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250715/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+
+    int A[1000];
+    A[0] = 0;
+    int AIndex = 1;
+    int totalTime = 0;
+
+    for (int i = 0; i < N; ++i)
+    {
+        int v, t;
+        cin >> v >> t;
+
+        for (int j = 0; j < t; ++j)
+        {
+            A[AIndex] = A[AIndex - 1] + v;
+            ++AIndex;
+        }
+
+        totalTime += t;
+    }
+
+    int B[1000];
+    B[0] = 0;
+    int BIndex = 1;
+
+    for (int i = 0; i < M; ++i)
+    {
+        int v, t;
+        cin >> v >> t;
+
+        for (int j = 0; j < t; ++j)
+        {
+            B[BIndex] = B[BIndex - 1] + v;
+            ++BIndex;
+        }
+    }
+
+    int whichIsFirst = 0;
+    int result = 0;
+
+    for (int i = 1; i <= totalTime; ++i)
+    {
+        if (A[i] < B[i] && whichIsFirst != -1)
+        {
+            whichIsFirst = -1;
+            ++result;
+        }
+        else if (A[i] > B[i] && whichIsFirst != 1)
+        {
+            whichIsFirst = 1;
+            ++result;
+        }
+        else if (A[i] == B[i] && whichIsFirst != 0)
+        {
+            whichIsFirst = 0;
+            ++result;
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
