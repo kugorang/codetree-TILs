ae99744d36f60f78ccdebb3a2c0efee9bf296fb7
Kim Hyeonwoo <ialskdji@gmail.com>
2025-07-19 22:35:08 +0900

    Title: 이상한 진수 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250719/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+int ConvertNum(string a)
+{
+    int result = 0;
+
+    for(char ch : a)
+        result = (result << 1) + (ch == '1' ? 1 : 0);
+    
+    return result;
+}
+
+int main()
+{
+    string a;
+    cin >> a;
+
+    int aLength = a.length();
+    int maxNum = INT_MIN;
+
+    for (int i = 1; i < aLength; ++i)
+    {
+        a[i] = (a[i] == '0') ? '1' : '0';
+        int aNum = ConvertNum(a);
+
+        if (aNum > maxNum)
+            maxNum = aNum;
+    }
+
+    cout << maxNum;
+
+    return 0;
+}
